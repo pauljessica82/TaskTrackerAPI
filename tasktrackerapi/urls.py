105baab3d973dd02576416ddb419d76d3eafc8e7
Jessica Paul <pauljessica82@gmail.com>
2022-02-04 15:20:58 -0500

    add url to serve react home

--- tasktrackerapi/urls.py
+++ tasktrackerapi/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 from tasktrackerapi.api.urls import router
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('tasks/', include('tasks.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 
