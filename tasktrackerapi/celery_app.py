0d766a038f14fd1b7e19caac48d2bff38b3a6604
Jessica Paul <pauljessica82@gmail.com>
2022-02-04 14:09:04 -0500

    commit updates

--- tasktrackerapi/celery_app.py
+++ tasktrackerapi/celery_app.py
@@ -4,9 +4,9 @@ from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasktrackerapi.settings')
-app = Celery('tasktrackerapi', broker=settings.CELERY_BROKER_URL)
+app = Celery('tasktrackerapi', broker=settings.CELERY_BROKER_URL,)
 app.conf.timezone = 'America/New_York'
-app.autodiscover_tasks()
+app.autodiscover_tasks(related_name="celery_tasks")
 
 app.conf.beat_schedule = {
     'reminder-time': {
