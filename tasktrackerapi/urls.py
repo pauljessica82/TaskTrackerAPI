ee5253a35a61141beb18870d0bbf301de82a8cb5
Jessica Paul <pauljessica82@gmail.com>
2022-03-17 15:54:51 -0400

    changes

--- tasktrackerapi/urls.py
+++ tasktrackerapi/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('tasks/', include('tasks.urls')),
-    path('', index, name='index'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
 
